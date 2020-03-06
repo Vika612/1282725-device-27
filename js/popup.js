cb245a9cb12ec5779519fdcca819be545a58073b
Vika612 <58486795+Vika612@users.noreply.github.com>
2020-03-07 00:15:57 +0100

    личный проект: начинаем оформление декоративных элементов

--- js/popup.js
+++ js/popup.js
@@ -3,18 +3,48 @@
 var button = document.querySelector(".write_us-btn");
 var popup = document.querySelector(".modal-write_us");
 var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
 var na_me = popup.querySelector("[name=fullname]");
+var ma_il = popup.querySelector("[name=mail]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem(na_me);
+} catch (err) {
+    isStorageSupport = false;
+}
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
 popup.classList.add("modal-show");
-na_me.focus();
+if (storage) {
+  na_me.value = storage;
+  ma_il.focus();
+} else {
+  na_me.focus();
+}
 });
+
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
 popup.classList.remove("modal-show");
 });
+
+
+form.addEventListener("submit", function (evt) {
+  if (!na_me.value || !ma_il.value) {
+  evt.preventDefault();
+  console.log("Введите Ваше имя и адрес электронной почты");
+  } else {
+      if (isStorageSupport) {
+        localStorage.setItem("fullname", na_me.value);
+    }
+  }
+});
+
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
